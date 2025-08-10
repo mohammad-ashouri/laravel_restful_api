e1a4771a81f7d173c9270290d8d0986c83e07b62
u226 <root@gitlab.local>
2025-08-10 11:27:41 +0330

    Make seeder for users (fake data)

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,10 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //})->middleware('auth:sanctum');
 
+Route::get('test', function () {
+    return response()->json([
+        'id' => 1,
+        'message' => 'fine!',
+        'numbers' => [1, 2, 3]
+    ]);
+});
