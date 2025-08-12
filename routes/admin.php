6fd47cb649a0395ea6efc4600073bcf9643a7e0b
u226 <root@gitlab.local>
2025-08-12 13:34:12 +0330

    Make article resource

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\ArticleController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('user', UserController::class);
+Route::apiResource('article', ArticleController::class)->only('index');
